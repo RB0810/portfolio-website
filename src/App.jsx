1b1a5c2b98b0f160a9b2fcbbef7267aac7a817cf
Rishika Banka <rishika0810banka@gmail.com>
2024-07-28 21:34:24 +0800

    checking something

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
       {showLoader ? (
-        <Loader />
+        <Home />
       ) : (
         <BrowserRouter basename="/portfolio-website">
           <Routes>
